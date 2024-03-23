f687cb843f70f162d624c88f3a2aa622fefb17fe
Femarko <fenstermark7@gmail.com>
2024-03-23 15:02:41 +0100

    Docker image does not want to be built

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -15,22 +15,31 @@ from pathlib import Path
 from dotenv import load_dotenv
 import environ
 
-load_dotenv()
+env = environ.Env(
+# set casting, default value
+#     DEBUG=(bool, False)
+)
+
+# load_dotenv()
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=hs6$#5om031nujz4staql9mbuste=!dc^6)4opsjq!vvjxzj@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
