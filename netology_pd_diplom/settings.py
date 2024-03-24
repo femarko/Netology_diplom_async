bc5fb7279389781c66875bc650f9e8bcf62e2e03
Femarko <fenstermark7@gmail.com>
2024-03-24 15:01:33 +0100

    celery: intermediary milestone

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -24,10 +24,10 @@ env = environ.Env(
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(env_file=Path('.env'))
 
 
 # Quick-start development settings - unsuitable for production
