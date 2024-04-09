4dfb9ac180dd568ffac1d28413e6e8981d0d5ec4
femarko <fenstermark7@gmail.com>
2024-04-09 03:33:00 +0200

    new_order_signal: celery-task

--- backend/signals.py
+++ backend/signals.py
@@ -51,20 +51,8 @@ def new_order_signal(user_id, **kwargs):
     """
     отправяем письмо при изменении статуса заказа
     """
-    # send an e-mail to the user
     # get params for the celery-task
     user = User.objects.get(id=user_id)
     email = user.email
     # call the celery-task
     new_order_signal_task.delay(email)
-    # msg = EmailMultiAlternatives(
-    #     # title:
-    #     f"Обновление статуса заказа",
-    #     # message:
-    #     'Заказ сформирован',
-    #     # from:
-    #     settings.EMAIL_HOST_USER,
-    #     # to:
-    #     [user.email]
-    # )
-    # msg.send()
