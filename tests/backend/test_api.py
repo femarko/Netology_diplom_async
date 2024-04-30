0e809880213a36e28634bc6f40ee626bddb1eb29
femarko <fenstermark7@gmail.com>
2024-04-30 14:44:26 +0200

    test_get_account_detailes, test_post_account_detailes pass

--- tests/backend/test_api.py
+++ tests/backend/test_api.py
@@ -1,5 +1,4 @@
 import pytest, copy, json
-from django.http import QueryDict
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 from django.http.response import JsonResponse
@@ -77,7 +76,7 @@ class TestUserRegisterConfirmLogin:
                 "json": {'Status': False, 'Errors': 'Не указаны все необходимые аргументы'}
             }
 
-    def test_register_account(self, user_data: user_data, path: Endpoint_path, client: client):
+    def test_register_account(self, user_data: user_data, path: Endpoint_path, client: client) -> None:
         response = client.post(path=path.user_register, data=user_data)
         user = User.objects.filter(email=user_data["email"]).first()
         print(f'{User.objects.filter(email=user_data["email"]).first().pk}')
@@ -99,7 +98,7 @@ class TestUserRegisterConfirmLogin:
         assert len(token) > 0
 
     @pytest.mark.usefixtures("user")
-    def test_confirm_account(self, client: client, path: Endpoint_path):
+    def test_confirm_account(self, client: client, path: Endpoint_path) -> None:
         user: User = self.user
         confirm_email_token_instance: ConfirmEmailToken = ConfirmEmailToken.objects.filter(user_id=user.pk).first()
         email_token: str = confirm_email_token_instance.key
@@ -112,7 +111,7 @@ class TestUserRegisterConfirmLogin:
         # assert user.is_active is True  # fails
 
     @pytest.mark.usefixtures("user")
-    def test_login(self, client: client, path: Endpoint_path, user_data: user_data):
+    def test_login(self, client: client, path: Endpoint_path, user_data: user_data) -> None:
         user: User = self.user
         email_token = ConfirmEmailToken.objects.filter(user_id=user.pk).first().key
         client.post(path=path.confirm_account, data={"email": user.email, "token": email_token})
@@ -127,16 +126,37 @@ class TestUserRegisterConfirmLogin:
         # assert user.is_active is True # fails
 
 
-class TestAccountDetailes:
+class TestAccountDetailesCategoryView:
 
     @pytest.mark.usefixtures("user")
-    def test_account_detailes(self, client: client, path: Endpoint_path, user_data: user_data):
+    def test_get_account_detailes(self, client: client, path: Endpoint_path, user_data: user_data) -> None:
         user: User = self.user
         client.force_authenticate(user=user)
         response: Response = client.get(path=path.user_details)
-        response_dict = json.loads(response.content)
-        user_data_dict = user_data
+        response_dict: dict = json.loads(response.content)
+        response_dict.pop("contacts")
+        response_dict.pop("id")
+        user_data_dict: dict = user_data
         user_data_dict.pop("password")
         assert response.status_code == 200
-        for key in user_data_dict:
-            assert user_data[key] == response_dict[key]
+        assert response_dict == user_data_dict
+
+    @pytest.mark.usefixtures("user")
+    def test_post_account_detailes(self, client: client, path: Endpoint_path, user_data: user_data) -> None:
+        user: User = self.user
+        user_data_amendments: dict = {
+            "first_name": "new_first_name",
+            "password": "new_password",
+            "email": "new_address@email.com"
+        }
+        user_data.update(user_data_amendments)
+        client.force_authenticate(user=user)
+        response_post: JsonResponse = client.post(path=path.user_details, data=user_data)
+        response_get: JsonResponse = client.get(path.user_details)
+        user_data.pop("password")
+        response_get_dict: dict = json.loads(response_get.content)
+        response_get_dict.pop("id")
+        response_get_dict.pop("contacts")
+        assert response_post.status_code == 200
+        assert response_post.json() == {'Status': True}
+        assert response_get_dict == user_data
